1a3979825deef8c012cf9ae473d7b8162967a91e
Joshua F. Rountree <joshua.rountree@scripps.com>
2019-02-15 14:08:30 -0500

    Updates verbiage for entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
  
-printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
+printf "\n\033[0;44mWorkbench Toolbelt is starting up...\033[0m\n"
 
 service ssh start
 service ssh status
+
+printf "\n\033[0;44mWill start other services here soon...\033[0m\n"
 
 exec "$@"
