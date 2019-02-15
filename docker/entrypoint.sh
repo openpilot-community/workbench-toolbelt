d4a054f603e4eb01cc4ddc876bd29446c357ebf2
Joshua F. Rountree <joshua.rountree@scripps.com>
2019-02-15 14:05:59 -0500

    Adds all the ZMQ ports and SSHD port, renames hostname to eon

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,5 @@ printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
 
 service ssh start
 service ssh status
-/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{printf $1}'
 
 exec "$@"
